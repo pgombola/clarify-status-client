32ee9a1ac576b8a9773dffd239b2b10cd1790748
pgombola <pgombola@cleo.com>
2017-03-30 16:25:41 -0400

    Added consul.addr command line arg; added job name to status request

--- src/main/java/com/cleo/clarify/server/status/StatusClient.java
+++ src/main/java/com/cleo/clarify/server/status/StatusClient.java
@@ -29,14 +29,15 @@ public class StatusClient {
 	}
 	
 	public List<Host> status() {
-		HostStatusRequest request = HostStatusRequest.newBuilder().build();
+		HostStatusRequest request = HostStatusRequest.newBuilder().setJobName("clarify").build();
 		HostStatusReply reply = blockingStatusStub.getHostStatus(request);
 		if (!reply.getError().isEmpty()) System.out.println(reply.getError());
 		return reply.getHostsList();
 	}
 
 	public static void main(String[] args) {
-		Consul consul = Consul.builder().withUrl("http://172.16.1.73:8500").build();
+		String consulAddr = findPropertyOrError(args, "consul.addr");
+		Consul consul = Consul.builder().withUrl(String.format("http://%s", consulAddr)).build();
 		ConsulResponse<List<CatalogService>> serviceResponse = consul.catalogClient().getService("clarify-status");
 		if (serviceResponse.getResponse().size() < 1) {
 			System.out.println("Couldn't find clarify-status service.");
@@ -47,6 +48,20 @@ public class StatusClient {
 		List<Host> hosts = client.status();
 		for (Host h : hosts) {
 			System.out.println(String.format("Hostname: %s; Status: %s", h.getHostname(), h.getStatus()));
+		}
+	}
+	
+	private static final String findPropertyOrError(String[] args, String property) {
+		String value = null;
+		for (String arg : args) {
+			if (arg.contains(property)){
+				value = arg.substring(arg.indexOf("=") + 1);
+			}
+		}
+		if (value == null || value.isEmpty()) {
+			throw new IllegalArgumentException(String.format("Must provide %s argument.", property));
+		} else {
+			return value;
 		}
 	}
 }
